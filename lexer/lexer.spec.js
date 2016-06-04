143b2457abe29e2a1ab54bbf473e988ab13231e4
Austin Adams <austin@farmingtonwebdesign.net>
2016-06-04 16:01:39 -0700

    Organize folders, add tests, add lexer functions

---
+++ lexer/lexer.spec.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const lexer = require('./lexer');
+
+
+const input = `
+
+
+
+
+
+
+
+`;
+
+
+
+it('Captures decisions', () => {
+  lexer.parseDecision()
+});
+
+// it('Captures Content groups as the groups in between decisions', () => {
+//  
+// });
+
